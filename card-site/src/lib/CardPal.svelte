f227ddb45e8544220265f19946afb89aa3d9c102
Marin Thomas <marin.f.thomas@gmail.com>
2024-05-21 17:43:12 +0200

    feat: basic static images

--- card-site/src/lib/CardPal.svelte
+++ card-site/src/lib/CardPal.svelte
@@ -7,8 +7,8 @@
 <div class="card w-96 bg-base-100 shadow-xl">
     <figure><img src={card.img} alt="Shoes" /></figure>
     <div class="card-body">
-      <h2 class="card-title">Shoes!</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <h2 class="card-title">{card.name}</h2>
+      <p>{card.description}</p>
       <div class="card-actions justify-end">
         <button class="btn btn-primary">Buy Now</button>
       </div>
--- card-site/src/routes/buy/+page.svelte
+++ card-site/src/routes/buy/+page.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
-    import Header from "$lib/Header.svelte";
-	import type { Card } from "../../models/Card";
+    import CardPal from "$lib/CardPal.svelte";
+import Header from "$lib/Header.svelte";
+	import { Card } from "../../models/Card";
     let cards: Card[] = [];
+    cards.push(new Card( 1, "Lamball", "Just a sheep", 1, "Neutral", 50, 25, 
+    "http://localhost:8080/lamball.jpg" ));
 </script>
 <Header/>
 
 <div class="card-wrapper">
     {#each cards as card }
-        <p>card</p>
+        <CardPal card = {card}/>
     {/each}
 </div>
 
