80dd087cc76ed31839c6770ed68f9ffdceffaf1e
Thomas0507 <marin.f.thomas@gmail.com>
2024-05-25 08:40:26 +0200

    feat: Data Injection on Startup

--- src/main/java/com/site/card/site/Application.java
+++ src/main/java/com/site/card/site/Application.java
@@ -1,13 +1,29 @@
 package com.site.card.site;
 
+import com.site.card.site.entities.Card;
+import com.site.card.site.repositories.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+
+	@Autowired
+	CardRepository cardRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Card lamball = new Card(1,"Lamball", "Just a sheep", "Neutral", "Neutral", 50, 50, 1);
+		Card cattiva = new Card(2,"Cattiva", "Just a cat", "Neutral", "Neutral", 50, 55, 2);
+		cardRepository.save(lamball);
+		cardRepository.save(cattiva);
+
+	}
 }
